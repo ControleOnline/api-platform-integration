e35f0f408e9f0b33179b67ba6289ee4404a10d76
Luiz Kim <luiz.kim@controleonline.com>
2025-04-22 20:52:05 -0300

    Update

--- src/Service/IntegrationService.php
+++ src/Service/IntegrationService.php
@@ -1,56 +1,60 @@
-<?php
+    <?php
 
-namespace ControleOnline\Service;
+    namespace ControleOnline\Service;
 
-use ControleOnline\Entity\Device;
-use ControleOnline\Entity\Integration;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface
-as Security;
-use ControleOnline\Service\StatusService;
+    use ControleOnline\Entity\Device;
+    use ControleOnline\Entity\Integration;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface
+    as Security;
+    use ControleOnline\Service\StatusService;
 
-class IntegrationService
-{
-    public function __construct(
-        private EntityManagerInterface $manager,
-        private Security $security,
-        private StatusService $statusService
-    ) {}
-
-
-    public function getOpenMessages(string $queueNane, ?Device $device = null): array
+    class IntegrationService
     {
-        $search = ['queueName' => $queueNane];
-        if ($device)
-            $search['device'] = $device;
+        public function __construct(
+            private EntityManagerInterface $manager,
+            private Security $security,
+            private StatusService $statusService
+        ) {}
 
-        return $this->manager->getRepository(Integration::class)->findBy($search);
+
+        public function getOpenMessages(string $queueNane, ?Device $device = null): array
+        {
+            $search = [
+                'queueName' => $queueNane,
+                'status' => $this->statusService->discoveryStatus('open', 'open', 'integration')
+            ];
+            
+            if ($device)
+                $search['device'] = $device;
+
+            return $this->manager->getRepository(Integration::class)->findBy($search);
+        }
+
+        public function setDelivered(Integration $integration)
+        {
+            $status = $this->statusService->discoveryStatus('closed', 'closed', 'integration');
+
+            $integration->setStatus($status);
+            $this->manager->persist($integration);
+            $this->manager->flush();
+
+            return $integration;
+        }
+
+        public function addIntegration(string $message, string $queueNane, ?Device $device): Integration
+        {
+            $status = $this->statusService->discoveryStatus('open', 'open', 'integration');
+
+            $integration = new Integration();
+            $integration->setDevice($device);
+            $integration->setStatus($status);
+            $integration->setQueueName($queueNane);
+            $integration->setBody($message);
+
+            $this->manager->persist($integration);
+            $this->manager->flush();
+
+            return $integration;
+        }
     }
-
-    public function setDelivered(Integration $integration)
-    {
-        $status = $this->statusService->discoveryStatus('closed', 'closed', 'integration');
-
-        $integration->setStatus($status);
-        $this->manager->persist($integration);
-        $this->manager->flush();
-
-        return $integration;
-    }
-
-    public function addIntegration(string $message, string $queueNane, ?Device $device): Integration
-    {
-        $status = $this->statusService->discoveryStatus('open', 'open', 'integration');
-
-        $integration = new Integration();
-        $integration->setDevice($device);
-        $integration->setStatus($status);
-        $integration->setQueueName($queueNane);
-        $integration->setBody($message);
-
-        $this->manager->persist($integration);
-        $this->manager->flush();
-
-        return $integration;
-    }
-}
