55c7836b185ee95c73e5d4faeee042df4bbc74fb
Luiz Kim <luiz.kim@controleonline.com>
2025-05-04 21:38:03 -0300

    Update

--- src/Entity/Integration.php
+++ src/Entity/Integration.php
@@ -116,7 +116,7 @@ class Integration
         return $this->user;
     }
 
-    public function setUser(User $user): self
+    public function setUser(?User $user): self
     {
         $this->user = $user;
         return $this;
