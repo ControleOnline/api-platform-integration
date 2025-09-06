d64c54ff93dfd75406cc7647ec36e4178be1ccab
Luiz Kim <luiz.kim@controleonline.com>
2025-09-06 18:49:53 -0300

    Bitcoin

--- src/Service/BitcoinService.php
+++ src/Service/BitcoinService.php
@@ -3,19 +3,19 @@
 namespace ControleOnline\Service;
 
 use ControleOnline\Entity\Invoice;
-use \Endroid\QrCode\Builder\Builder;
-use \Endroid\QrCode\Encoding\Encoding;
-use \Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\Writer\PngWriter;
 
 class BitcoinService
 {
     public function getBitcoin(Invoice $invoice): array
     {
         $walletAddress = 'BC1QQDMP6P903LNVLQRJ7QWMSGWX7D8YLTQURZDMPL';
-        $amount = $invoice->getPrice(); // Valor em BTC
+        $amount = $invoice->getPrice();
         $payload = "bitcoin:{$walletAddress}?amount={$amount}";
 
-        $qrCode = Builder::create()
+        $qrCode = Builder::fromScratch()
             ->writer(new PngWriter())
             ->data($payload)
             ->encoding(new Encoding('UTF-8'))
