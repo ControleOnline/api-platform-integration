dfa21c2a75f10facfeb9c199573f1f35ae7b2e41
Luiz Kim <luiz.kim@controleonline.com>
2026-01-26 18:54:33 -0300

    99Food

---
+++ src/Controller/99Food/WebhookController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ControleOnline\Controller\iFood;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use ControleOnline\Service\IntegrationService;
+use ControleOnline\Service\LoggerService;
+
+class WebhookController extends AbstractController
+{
+    protected static $logger;
+
+    public function __construct(
+        private LoggerService $loggerService,
+    ) {
+        self::$logger = $loggerService->getLogger('99Food');
+    }
+
+    #[Route('/webhook/99food', name: '99food_webhook', methods: ['POST'])]
+    public function handle99FoodWebhook(
+        Request $request,
+        IntegrationService $integrationService
+    ): Response {
+        $rawInput = $request->getContent();
+        $signature = $request->headers->get('didi-header-sign');
+
+        $event = json_decode($rawInput, true);
+
+        $integrationService->addIntegration($rawInput, '99Food');
+        self::$logger->info('Evento enviado para a fila', ['event' => $event]);
+
+        return new Response('[accepted]', Response::HTTP_ACCEPTED);
+    }
+}
