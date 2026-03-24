01b5052b2604364fb02dca6fdd9d5c81645fa4a1
Luiz Kim <luiz.kim@controleonline.com>
2026-03-24 10:32:23 -0300

    WhatsAppContent

--- src/MessageHandler/SendAutomationMessageHandler.php
+++ src/MessageHandler/SendAutomationMessageHandler.php
@@ -6,6 +6,7 @@ use ControleOnline\Entity\Connection;
 use ControleOnline\Entity\Task;
 use ControleOnline\Message\SendAutomationMessage;
 use ControleOnline\Service\N8NService;
+use ControleOnline\WhatsApp\Messages\WhatsAppContent;
 use ControleOnline\WhatsApp\Messages\WhatsAppMessage;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -23,11 +24,14 @@ class SendAutomationMessageHandler
         $connection = $this->em->getRepository(Connection::class)->find($message->connectionId);
         $task = $this->em->getRepository(Task::class)->find($message->taskId);
 
+        $messageContent = new WhatsAppContent();
+        $messageContent->setBody($message->messageData['message']);
+
         $whatsAppMessage = new WhatsAppMessage();
         $whatsAppMessage->setAction($message->messageData['action']);
         $whatsAppMessage->setOriginNumber($message->messageData['origin']);
         $whatsAppMessage->setDestinationNumber($message->messageData['destination']);
-        $whatsAppMessage->setMessageContent($message->messageData['message']);
+        $whatsAppMessage->setMessageContent($messageContent);
 
         $this->n8nService->sendToWebhook(
             $whatsAppMessage,
